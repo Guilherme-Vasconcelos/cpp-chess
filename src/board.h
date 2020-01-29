caa7c1b4860ed34253a20e74e1a4c553cfd8882c
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2020-01-28 22:47:12 -0300

    Added documentation

--- src/board.h
+++ src/board.h
@@ -15,16 +15,72 @@
 #include <vector>
 #include "pieces.h"
 
+/**
+ * Board class contains necessary methods to deal
+ * with boards, setting pieces, etc.
+ */
 class Board {
 private:
+    /**
+     * currentFen stores the current FEN value 
+     * It is a std::string type with the current game's FEN
+     * setter: setFenPosition (see below)
+     * getter: getCurrentFen (see below)
+     */
     std::string currentFen;
 public:
     Board();
+
+    /**
+     * 8x8 matrix of Pieces to represent the chess board
+     * setter: place (see below)
+     * getter: get (see below)
+     */
     std::vector<std::vector<Piece>> pieces;
+
+    /**
+     * Method used to show the current board. Example:
+     *     Board.showBoard();
+     * @return void
+     */
     void showBoard();
+
+    /**
+     * Method used to place a piece at the board instance (using algebraic notation)
+     * Example:
+     *     Board.place(bishop, 'h', 1);
+     * @param piece Piece (or any derived class, such as Bishop) instance
+     * @param positionY a char from a to h containing the column you want to place the piece
+     * @param positionX an int from 1 to 8 containing the row you want to place the piece
+     * @return void
+     */
     void place(Piece piece, char positionY, int positionX);
+
+    /**
+     * Method used to find out which piece is at a given position (using algebraic notation)
+     * Example: 
+     *     Board.get('a', 1);
+     * @param positionY a char from a to h containing the column you want to analyze
+     * @param positionX an int from 1 to 8 containing the row you want to analyze
+     * @return Piece (or any derived class) instance that is in the given position
+     */
     Piece get(char positionY, int positionX);
+
+    /**
+     * Method used to set a custom FEN position
+     * Example: 
+     *     Board.setFenPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+     * @param fen a std::string representing the FEN you want to set
+     * @return void
+     */
     void setFenPosition(std::string fen);
+
+    /**
+     * Method used to get the current position's FEN
+     * Example: 
+     *     Board.getCurrentFen();
+     * @return std::string with current position's FEN
+     */
     std::string getCurrentFen();
 };
 
--- src/pieces.h
+++ src/pieces.h
@@ -13,53 +13,131 @@
 
 #include <iostream>
 
+/**
+ * class Color is used to define each piece's color (see class Piece below)
+ */
 enum class Color {white, black};
 
+/**
+ * class Piece is used as a base class for class Rook, Bishop, etc.
+ */
 class Piece {
 protected:
+    /**
+     * name stores a letter that represents the piece
+     * uppercase letters represent white pieces
+     * and lowercase letters represent black pieces, following FEN's convention
+     * P for pawn, K for king, Q for queen, R for rook, B for bishop, N for knight
+     */
     std::string name;
+
+    /**
+     * Each position of the board must have a piece. Because of this,
+     * we have a nullPiece attribute, which is used to check whether the object
+     * belongs to class NullPiece or not. NullPieces do nothing, they are just
+     * placeholders for empty board positions.
+     */
     bool nullPiece;
+
+    /**
+     * Attribute color stores the piece's color (white or black)
+     * it belongs to enum class Color, defined above
+     */
     Color color;
 public:
     Piece();
     int positionX;
     int positionY;
+
+    /**
+     * Method getName is used to get the name of the piece
+     * Example:
+     *     Bishop bishop = Bishop(Color::white);
+     *     bishop.getName();
+     * @return std::string
+     */
     std::string getName();
+
+    /**
+     * Method getColor is used to get the color of a piece
+     * Example:
+     *     Queen queen = Queen(Color::black);
+     *     queen.getColor();
+     * @return Color
+     */
     Color getColor();
+
+    /**
+     * Method isNullPiece is a getter for the attribute nullPiece
+     * Example:
+     *     King king = King(Color::white);
+     *     kinng.isNullPiece();
+     * @return bool
+     */
     bool isNullPiece();
+
     virtual ~Piece() {};
 };
 
+/**
+ * class Rook inherits from Piece
+ * and is used to instance rooks
+ */
 class Rook: public Piece {
 public:
     Rook(Color color);
 };
 
+/**
+ * class Bishop inherits from Piece
+ * and is used to instance bishops
+ */
 class Bishop: public Piece {
 public:
     Bishop(Color color);
 };
 
+/**
+ * class Knight inherits from Piece
+ * and is used to instance knights
+ */
 class Knight: public Piece {
 public:
     Knight(Color color);
 };
 
+/**
+ * class King inherits from Piece
+ * and is used to instance kings
+ */
 class King: public Piece {
 public:
     King(Color color);
 };
 
+/**
+ * class Queen inherits from Piece
+ * and is used to instance queens
+ */
 class Queen: public Piece {
 public:
     Queen(Color color);
 };
 
+/**
+ * class Pawn inherits from Piece
+ * and is used to instance pawns
+ */
 class Pawn: public Piece {
 public:
     Pawn(Color color);
 };
 
+/**
+ * class NullPiece inherits from Piece
+ * and is used to instance nullPieces, which are
+ * placeholders for empty positions at the chess board
+ */
 class NullPiece: public Piece {
 public:
     NullPiece();
